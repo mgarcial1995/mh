eda17bac55ea7ed9597da5537ecebc73a0557364
Martin <martingarcial.1995@gmail.com>
2020-07-13 22:16:43 -0500

    Maquetacion y estructurado 90%

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,188 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    comerce: [
+      {
+        name: "Mangueras hidráulicas y accesorios",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Repuestos para perforadora",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Niples escamados de todas las medidas",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Brocas de perforación o pepa de rueda",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Barrenos",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Ventiladores para minería artesanal",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Winches ",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Mangueras para aire",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Acoples para manguera de aire ",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Carros mineros ",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Carretillas ",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Pulmones para aire, minería artesanal",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Purgadores para aire",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Repuestos para carro minero",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Abrasivos",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Soldaduras",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Pernos y medidas",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Poleas acanaladas de aluminio",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+    ],
+    reparation: [
+      {
+        name: "Carros mineros",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Winches",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Ventiladores",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name:
+          "Estructuras de los equipos tracles, cargador frontal y volquetes",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Cucharones de scoop ",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Reparación de cucharones de cargador frontal",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Reparación de ruedas de carro minero",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Reparación de ruedas de locomotora",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+    ],
+    fabrication: [
+      {
+        name: "Carros mineros",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Techos metálicos",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Cercos perimétricos",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Poleas acanaladas",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Ejes, pines, bocinas",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Pernos y tuercas",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Tanques metálicos",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Cambios para línea riel ",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Monas fijas ",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Monas manuales",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Estructuras metálicas",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Ventiladores axiales",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Ventiladores tipo caracol",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Carretillas",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Purgadores para aire",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Botellas para agua (PERFORACIÓN)",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+      {
+        name: "Pulmones para aire (PERFORACIÓN)",
+        image: require("../assets/img/services/comerce/service.jpg"),
+      },
+    ],
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
 });
--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,11 +1,78 @@
 <template>
   <div class="services">
     <div class="services__header">
-      <div class="services__header__text ">
+      <div class="services__header__text">
         <p class="services__header__text--title">Servicios</p>
-        <p class="services__header__text--text">
-          El mejor servicio con la mejor calidad a nivel nacional</p>
+        <p
+          class="services__header__text--text"
+        >El mejor servicio con la mejor calidad a nivel nacional</p>
+      </div>
+    </div>
+    <p class="title">Nuestros productos y servicios</p>
+    <div class="section__services">
+      <div class="section__services__navServices">
+        <p
+          @click="showComerce = true, showFabrication = false, showReparation = false"
+          :class="{colorWhite: showComerce}"
+          class="section__services__navServices--item"
+        >COMERCIO</p>
+        <p
+          @click="showComerce = false, showFabrication = true, showReparation = false"
+          :class="{colorWhite: showFabrication}"
+          class="section__services__navServices--item"
+        >FABRICACIÓN</p>
+        <p
+          @click="showComerce = false, showFabrication = false, showReparation = true"
+          :class="{colorWhite: showReparation}"
+          class="section__services__navServices--item"
+        >REPARACIÓN</p>
+      </div>
+
+      <div v-if="showComerce" class="comerce__service">
+        <Service
+          v-for="(service, index) in comerce"
+          :key="index"
+          :name="service.name"
+          :image="service.image"
+        />
+      </div>
+      <div v-if="showFabrication" class="fabrication__service">
+        <Service
+          v-for="(service, index) in reparation"
+          :key="index"
+          :name="service.name"
+          :image="service.image"
+        />
+      </div>
+      <div v-if="showReparation" class="reparation__service">
+        <Service
+          v-for="(service, index) in fabrication"
+          :key="index"
+          :name="service.name"
+          :image="service.image"
+        />
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import Service from "@/components/Service";
+import { mapState } from "vuex";
+export default {
+  name: "Services",
+  components: {
+    Service
+  },
+  data() {
+    return {
+      showComerce: true,
+      showFabrication: false,
+      showReparation: false
+    };
+  },
+  computed: {
+    ...mapState(["comerce", "reparation", "fabrication"])
+  }
+};
+</script>
